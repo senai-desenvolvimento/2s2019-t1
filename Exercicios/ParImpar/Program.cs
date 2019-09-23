7dcba0374b79fc0aa626567fef92b0ff3265f4e7
Alexandre Fukaya <afukaya@hotmail.com>
2019-09-23 13:28:56 -0300

    Exercicios 20/09

---
+++ Exercicios/ParImpar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do{
+                Console.Write("Diga um número ou zero para terminar: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num != 0){
+                    if (num % 2 == 0){
+                        Console.WriteLine("O número é par");
+                    } else {
+                        Console.WriteLine("O número é impar");
+                    }
+                }
+            } while(num != 0);
+        }
+    }
+}
---
+++ Exercicios/contaimpar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace contaimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i+=2){
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
